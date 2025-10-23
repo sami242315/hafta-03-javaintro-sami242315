fb82a78c7669842bcc2645944179895796ef2327
sami242315 <250541081@firat.edu.tr>
2025-10-23 17:15:09 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,13 @@
+public class Hosgeldiniz {
+    public static void main(String[] args) {
+        System.out.println("Java'ya hoş geldiniz");
+    }
+}public class BilgisayarBilimineHosgeldiniz {
+    public static void main(String[] args) {
+        System.out.println("Bilgisayar bilimine hoş geldiniz");
+    }
+}public class ProgramlamaEglencelidir {
+    public static void main(String[] args) {
+        System.out.println("Programlama eğlencelidir");
+    }
+}
