49ac4a4e50a4b66acc162d15ebebdeae72eebe95
sami242315 <250541081@firat.edu.tr>
2025-10-23 17:37:57 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class StudentCard {
+    public static void main(String[] args) {
+        String name = "Sami Yusuf";
+        String surname = "Yıldız";
+        String studentNumber = "250541081";
+        String department = "Yazılım Mühendisliği";
+
+        System.out.println("********** ÖĞRENCİ KARTI **********");
+        System.out.println("İsim       : " + name);
+        System.out.println("Soyad      : " + surname);
+        System.out.println("Öğrenci No : " + studentNumber);
+        System.out.println("Bölüm      : " + department);
+        System.out.println("************************************");
+    }
+}
