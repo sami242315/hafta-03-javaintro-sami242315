d42e3946ed084dc0e3631fe78de136d97db0fd16
sami242315 <250541081@firat.edu.tr>
2025-10-23 17:18:20 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Faktoriyel {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = input.nextInt();
+
+        int faktoriyel = 1;
+
+        for (int i = 1; i <= sayi; i++) {
+            faktoriyel *= i;
+        }
+
+        System.out.println(sayi + "! = " + faktoriyel);
+    }
+}
