0054b9ef3744cf3c52ebfddf5f3a6f216e4752af
sami242315 <250541081@firat.edu.tr>
2025-10-23 17:32:35 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,6 @@
+public class SumExample {
+    public static void main(String[] args) {
+        int sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        System.out.println("Toplam: " + sum);
+    }
+}
