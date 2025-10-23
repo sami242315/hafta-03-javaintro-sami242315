0122388d1e75ceaff25c619df7e5e22e432546c5
sami242315 <250541081@firat.edu.tr>
2025-10-23 17:04:40 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,7 @@
+public class Hosgeldiniz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Java'ya hoş geldiniz");
+        }
+    }
+}
